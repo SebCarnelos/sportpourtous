adf87de2ceccb1627228955580114d962248fb8e
SebCarnelos <websebs@gmail.Com>
2017-08-13 19:29:26 +0200

    Desktop navbar logo scrolling, links left

---
+++ javascript/menu.js
@@ -0,0 +1,29 @@
+// Mobile menu
+function toggleMenu() {
+  var mobileMenu = document.getElementById('mobile-menu');
+  if(mobileMenu.style.display == "block") { // if is mobileMenu displayed, hide it
+    mobileMenu.style.display = "none";
+  }
+  else { // if is mobileMenu hidden, display it
+    mobileMenu.style.display = "block";
+  }
+}
+
+
+// Logo Rises for desktop menu
+var setSkrollr = function($el, data) {
+  for (var i = 0, l = data.length; i < l; i++) {
+      var d = data[i],
+          px = d[0];
+          css = d[1];
+      $el.attr('data-' + px, css);
+  }
+}
+
+jQuery(function($) {
+  setSkrollr($('#logorise'), [[0, 'transform:translateY(-100%)'], [0, 'transform:translateY(-37px)'], [600, 'transform:translateY(-100%)']]);
+  
+  skrollr.init({
+      smoothScrolling: false
+  });
+});
--- javascript/mobile-menu.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  var mobileMenu = document.getElementById('mobile-menu');
-  if(mobileMenu.style.display == "block") { // if is mobileMenu displayed, hide it
-    mobileMenu.style.display = "none";
-  }
-  else { // if is mobileMenu hidden, display it
-    mobileMenu.style.display = "block";
-  }
-}
