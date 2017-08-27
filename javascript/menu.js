b66c7ea4f8d1314a5cdba2f55164738cd8dc7045
SebCarnelos <websebs@gmail.com>
2017-08-27 16:25:45 +0200

    Smooth back to top javascript

--- javascript/menu.js
+++ javascript/menu.js
@@ -28,3 +28,13 @@ if ( $(window).width() > 540 ) {
     });
   });
   }
+
+  // Smooth scroll to top
+  var timeOut;
+  function scrollToTop() {
+    if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
+      window.scrollBy(0,-50);
+      timeOut=setTimeout('scrollToTop()',10);
+    }
+    else clearTimeout(timeOut);
+  }
